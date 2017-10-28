a02570098e3585261d1e1e926e1227fc9f61854f
Alex Szarka <xavvior@gmail.com>
2017-10-28 11:47:38 +0200

    Remove auth datas

--- config.skel.php
+++ config.skel.php
@@ -9,10 +9,10 @@
  * @link     https://github.com/varosifak/api
  */
 define("DB_TYPE",               "mysql");
-define("DB_HOST",               "localhost");
-define("DB_USER",               "root");
-define("DB_PASS",               "Qwedesdayztghui2");
-define("DB_NAME",               "varosifak_dev2");
+define("DB_HOST",               "");
+define("DB_USER",               "");
+define("DB_PASS",               "");
+define("DB_NAME",               "");
 define("DB_CHARSET",            "utf8");
 define("MAINTENANCE",           false);
 ?>
